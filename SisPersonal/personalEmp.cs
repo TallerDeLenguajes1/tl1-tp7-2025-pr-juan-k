0fb5b932893c5e4733c16d03a39974b7a483cbf2
pr-juan-k <djjuankalexis18@gmail.com>
2025-06-02 15:41:56 -0300

    avance 2

---
+++ SisPersonal/personalEmp.cs
@@ -0,0 +1,22 @@
+namespace gestionEpresa
+{
+    public class Empleados
+    {
+        enum cargos
+        {
+            Auxiliar,
+            Administrativo,
+            Ingeniero,
+            Especialista,
+            Investigador
+        }
+        private string nombre;
+        private string apellido;
+        private dateTimee fechaDeNacimiento;
+        private char estadoCivil;
+        private dateTimeee fechaIngreso
+        private double sueldoBasico
+        private cargos cargo;
+    }
+}
+
